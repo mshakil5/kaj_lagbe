bf351c337fbb51458b9b14c884692151827d71ed
shakil <kazimuhammadullah@gmail.com>
2024-02-09 01:46:08 +0600

    kaj

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,7 +15,7 @@
             <div class="inner">
               <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-              <p>Bounce Rate</p>
+              <p>Some text will be there</p>
             </div>
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
@@ -30,7 +30,7 @@
             <div class="inner">
               <h3>44</h3>
 
-              <p>User Registrations</p>
+              <p>Some text will be there</p>
             </div>
             <div class="icon">
               <i class="ion ion-person-add"></i>
