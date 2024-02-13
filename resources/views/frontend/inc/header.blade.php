aad0a6c44ac12998caef6256819b5bb3868cddf6
shakil <kazimuhammadullah@gmail.com>
2024-02-13 15:02:25 +0600

    kaj

--- resources/views/frontend/inc/header.blade.php
+++ resources/views/frontend/inc/header.blade.php
@@ -1,49 +1,5 @@
-<header class="site-header">
-    <div class="container">
-        <div class="row">
 
-            <div class="col-lg-8 col-12 d-flex flex-wrap">
-                <p class="d-flex me-4 mb-0">
-                    <i class="bi-geo-alt me-2"></i>
-                    Address here
-                </p>
 
-                <p class="d-flex mb-0">
-                    <i class="bi-envelope me-2"></i>
-
-                    <a href="mailto:info@company.com">
-                        Email here
-                    </a>
-                </p>
-            </div>
-
-            <div class="col-lg-3 col-12 ms-auto d-lg-block d-none">
-                <ul class="social-icon">
-                    <li class="social-icon-item">
-                        <a href="#" class="social-icon-link bi-twitter"></a>
-                    </li>
-
-                    <li class="social-icon-item">
-                        <a href="#" class="social-icon-link bi-facebook"></a>
-                    </li>
-
-                    <li class="social-icon-item">
-                        <a href="#" class="social-icon-link bi-instagram"></a>
-                    </li>
-
-                    <li class="social-icon-item">
-                        <a href="#" class="social-icon-link bi-youtube"></a>
-                    </li>
-
-                    <li class="social-icon-item">
-                        <a href="#" class="social-icon-link bi-whatsapp"></a>
-                    </li>
-                </ul>
-            </div>
-
-        </div>
-    </div>
-</header>
 
 <nav class="navbar navbar-expand-lg bg-light shadow-lg">
     <div class="container">
