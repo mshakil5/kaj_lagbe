14eba47d08007f08c57a8138b0de59c9ac4d4a64
shakil <kazimuhammadullah@gmail.com>
2024-05-14 23:00:45 +0600

    job order mail

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -117,12 +117,13 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="{{ asset('assets/admin/js/jquery.min.js')}}"></script>
+{{-- <script src="{{ asset('assets/admin/js/jquery.min.js')}}"></script> --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{ asset('assets/admin/js/jquery-ui.min.js')}}"></script>
+{{-- <script src="{{ asset('assets/admin/js/jquery-ui.min.js')}}"></script> --}}
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-  $.widget.bridge('uibutton', $.ui.button)
+  // $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('assets/admin/js/bootstrap.bundle.min.js')}}"></script>
--- resources/views/admin/work/new.blade.php
+++ resources/views/admin/work/new.blade.php
@@ -28,68 +28,71 @@
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-              <table id="example1" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th>Sl</th>
-                  <th>Date</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Phone</th>
-                  <th>Address</th>
-                  <th>Status</th>
-                  <th>Details</th>
-                </tr>
-                </thead>
-                <tbody>
-                  @foreach ($data as $key => $data)
+              <div class="table-responsive">
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
                   <tr>
-                    <td>{{ $key + 1 }}</td>
-                    <td>{{ \Carbon\Carbon::parse($data->date)->format('d/m/Y') }}</td>
-                    <td>{{$data->name}}</td>
-                    <td>{{$data->email}}</td>
-                    <td>{{$data->phone}}</td>
-                    <td>
-                        {{$data->address_first_line}} </br>
-                        {{$data->address_second_line}}</br>
-                        {{$data->address_third_line}}</br>
-                        {{$data->town}}</br>
-                        {{$data->post_code}}
-                    </td>
-                    <td>
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-secondary">
-                          <span id="stsval{{$data->id}}">
-                          @if ($data->status == 1) New
-                          @elseif($data->status == 2) In progress
-                          @elseif($data->status == 3) Completed
-                          @elseif($data->status == 4) Cancelled
-                          @endif
-                        </span>
-                      </button>
-                        <button type="button" class="btn btn-secondary dropdown-toggle dropdown-hover dropdown-icon" data-toggle="dropdown">
-                          <span class="sr-only">Toggle Dropdown</span>
-                        </button>
-                        <div class="dropdown-menu" role="menu">
-                          <a class="dropdown-item stsBtn" style="cursor: pointer;" data-id="{{$data->id}}" value="1" >New</a>
-                          <a class="dropdown-item stsBtn" style="cursor: pointer;" data-id="{{$data->id}}" value="2">In Progress</a>
-                          <a class="dropdown-item stsBtn" style="cursor: pointer;" data-id="{{$data->id}}" value="3">Completed</a>
-                          <a class="dropdown-item stsBtn" style="cursor: pointer;" data-id="{{$data->id}}" value="4">Cancelled</a>
-                        </div>
-                      </div>
-                    </td>
-                   
-                    <td>
-                        <a href="{{ route('admin.work.details', $data->id) }}" class="btn btn-secondary">
-                            <i class="fas fa-eye"></i>
-                        </a>
-                    </td>
-
+                    <th>Sl</th>
+                    <th>Date</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Status</th>
+                    <th>Details</th>
                   </tr>
-                  @endforeach
-                
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    @foreach ($data as $key => $data)
+                    <tr>
+                      <td>{{ $key + 1 }}</td>
+                      <td>{{ \Carbon\Carbon::parse($data->date)->format('d/m/Y') }}</td>
+                      <td>{{$data->name}}</td>
+                      <td>{{$data->email}}</td>
+                      <td>{{$data->phone}}</td>
+                      <td>
+                          {{$data->address_first_line}} </br>
+                          {{$data->address_second_line}}</br>
+                          {{$data->address_third_line}}</br>
+                          {{$data->town}}</br>
+                          {{$data->post_code}}
+                      </td>
+                      <td>
+                        <div class="btn-group">
+                          <button type="button" class="btn btn-secondary">
+                            <span id="stsval{{$data->id}}">
+                            @if ($data->status == 1) New
+                            @elseif($data->status == 2) In progress
+                            @elseif($data->status == 3) Completed
+                            @elseif($data->status == 4) Cancelled
+                            @endif
+                          </span>
+                        </button>
+                          <button type="button" class="btn btn-secondary dropdown-toggle dropdown-hover dropdown-icon" data-toggle="dropdown">
+                            <span class="sr-only">Toggle Dropdown</span>
+                          </button>
+                          <div class="dropdown-menu" role="menu">
+                            <a class="dropdown-item stsBtn" style="cursor: pointer;" data-id="{{$data->id}}" value="1" >New</a>
+                            <a class="dropdown-item stsBtn" style="cursor: pointer;" data-id="{{$data->id}}" value="2">In Progress</a>
+                            <a class="dropdown-item stsBtn" style="cursor: pointer;" data-id="{{$data->id}}" value="3">Completed</a>
+                            <a class="dropdown-item stsBtn" style="cursor: pointer;" data-id="{{$data->id}}" value="4">Cancelled</a>
+                          </div>
+                        </div>
+                      </td>
+                     
+                      <td>
+                          <a href="{{ route('admin.work.details', $data->id) }}" class="btn btn-secondary">
+                              <i class="fas fa-eye"></i>
+                          </a>
+                      </td>
+  
+                    </tr>
+                    @endforeach
+                  
+                  </tbody>
+                </table>
+              </div>
+              
             </div>
             <!-- /.card-body -->
           </div>
@@ -108,8 +111,15 @@
 @section('script')
 
 <script>
+
+  $(document).ready(function () {
+          $('#example1').DataTable();
+      });
+
+
+
   $(function () {
-    $("#example1").DataTable({
+    $("#example1cdxsr").DataTable({
       "responsive": true, "lengthChange": false, "autoWidth": false,
       "buttons": ["copy", "csv", "excel", "pdf", "print"]
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
@@ -130,8 +140,7 @@
     
     
     $('.stsBtn').click(function() {
-      alert('btn work');
-      // var url = "{{URL::to('/admin/change-client-status')}}";
+      var url = "{{URL::to('/admin/change-client-status')}}";
       var id = $(this).data('id');
       var status = $(this).attr('value');
       $.ajax({
